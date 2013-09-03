5db2f25aaeb5ea0c86006a334e85adffc723a52a
J. Fernando Sánchez <balkian@gmail.com>
2013-09-03 17:31:47 +0200

    Minor patch client

--- javascript/client/test/browser-test.js
+++ javascript/client/test/browser-test.js
@@ -1,6 +1,7 @@
 #!/bin/env node
-var connect = require('connect');
+var connect = require('connect'),
+    path = require('path');
 connect.createServer(
-    connect.static(__dirname)
+    connect.static(path.join(__dirname,'../'))
 ).listen(9090);
 
